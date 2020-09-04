d9e4a27b4d3530991ff6e86633d63c2e0da7d9e2
manuelesp <manuel2601.41@gmail.com>
2020-09-03 23:58:05 -0500

    pagina principal

--- index.php
+++ index.php
@@ -6,6 +6,24 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <nav class="menu">
+        <ul>
+            <li>inicio</li>
+            <li>nosotros</li>
+            <li>contactenos</li>
+            <li>login</li>
+        </ul>
+    </nav>
+    <section class="mensaje">
+        <article>
+            <h2>Matricula 2021</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, provident rerum? Eum maxime mollitia eaque doloribus architecto iste! Eligendi qui adipisci facere ratione et nisi sit explicabo veritatis at inventore.</p>
+        </article>
+    </section>
+    <footer>
+        <p>redes sociales</p>
+        <p>direccion</p>
+        <p>horarios de atencion</p>
+    </footer>
 </body>
 </html>
