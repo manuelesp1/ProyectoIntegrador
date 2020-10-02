af53f5bc7a5e59b1bd263ea67e7535f190d399ce
Luis-CR <70856744+Luis-CR@users.noreply.github.com>
2020-10-02 18:27:50 -0500

    boots

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="vista/css/principal.css">
+    <link rel="stylesheet" href="Vista/css/principal.css">
+    <link rel="stylesheet" href="Vista/css/bootstrap.css">
     <title>Colegio Letona</title>
 </head>
 <body>
