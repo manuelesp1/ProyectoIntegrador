b94fafb4cfd80082fedddb54bd9a1d9c2e2217c4
Luis-CR <70856744+Luis-CR@users.noreply.github.com>
2020-10-11 15:43:55 -0500

    val

--- Vista/js/vali.js
+++ Vista/js/vali.js
@@ -29,7 +29,7 @@ function checkInputs(){
     
 }
 */
-function validar() {
+function validar(){
 var nombre = document.getElementById('nombre').value;
 var apepat = document.getElementById('apepat').value;
 var apemat = document.getElementById('apemat').value;
@@ -39,66 +39,57 @@ var error = document.getElementById('error').value;
 var expresion = /\w+@\w+\.+[a-z]/;
 var letras = /[a-zA-ZÀ-ÿ\s]/;
 var numero = /[0-9]/;
-error.style.color='red';
+
 var mensajeError=[];
+var cont=0;
 if (nombre ===""||apepat ===""||apemat ===""||correo ===""||contraseña ==="") {
-    alert("Todos los campos son obligatorios");
-    mensajeError.push('Todos los campos son obligatorios');
+    mensajeError.push('Todos los campos son obligatorios<br>');
+    document.getElementById("error").innerHTML = "Todos los campos son obligatorios<br>";
     return false;
+    
 } 
 /*nombre*/
 else if(nombre.length>15){
-    alert("El nombre es muy largo");
-    mensajeError.push('El nombre es muy largo');
-    error.innerHTML=mensajeError.join(', ');
+    document.getElementById("error").innerHTML = "El nombre es muy largo";
+    mensajeError[cont]="Todos los campos son obligatorios<br>";
     return false;
 }
-else if(!letras.test(nombre)){    
-    mensajeError.push('El nombre solo debe contener letras,,,,,');
-    error.innerHTML=mensajeError.join(', ');
+else if(!letras.test(nombre)){        
+    document.getElementById("error").innerHTML = "El nombre solo debe contener letras";
     return false;
 }
 /*apellido paterno*/
 else if(apepat.length>15){
-    alert("El apellido paterno es muy largo");
-    mensajeError.push('El nombre es muy largo');
-    error.innerHTML=mensajeError.join(', ');
+    //alert("El apellido paterno es muy largo");
+    document.getElementById("error").innerHTML = "El apellido paterno es muy largo";
     return false;
 }
 else if(!letras.test(apepat)){
-    alert("El apellido paterno debe ser solo letras");
-    mensajeError.push('El nombre es muy largo');
-    error.innerHTML=mensajeError.join(', ');
+    //alert("El apellido paterno debe ser solo letras");
+    document.getElementById("error").innerHTML = "El apellido paterno debe ser solo letras";
     return false;
 }
 /*apellido materno*/
 else if(apemat.length>15){
-    alert("El apellido paterno es muy largo");
-    mensajeError.push('El nombre es muy largo');
-    error.innerHTML=mensajeError.join(', ');
+    document.getElementById("error").innerHTML = "El apellido materno es muy largo";
     return false;
 }
 else if(!letras.test(apemat)){
-    alert("El apellido materno debe ser solo letras");
-    mensajeError.push('El nombre es muy largo');
-    error.innerHTML=mensajeError.join(', ');
+    document.getElementById("error").innerHTML = "El apellido materno  debe ser solo letras";
     return false;
 }
 /*Correo*/
 else if(correo.length>20){
-    alert("El correo es muy largo");
-    mensajeError.push('El nombre es muy largo');
+    document.getElementById("error").innerHTML = "El correo es muy largo";
     return false;
 }
 else if(!expresion.test(correo)){
-    alert("El correo debe tener este formato ejemplo@ejemplo.com");
-    mensajeError.push('El nombre es muy largo');
+    document.getElementById("error").innerHTML = "El correo debe tener este formato ejemplo@ejemplo.com";
     return false;
 }
 /*contraseña*/
 else if(contraseña.length<8&&contraseña.length<16){
-    alert("El contraseña es muy corta minimo 8 caracteres\n");
-    mensajeError.push('El nombre es muy largo');
+    document.getElementById("error").innerHTML = "El contraseña es muy corta minimo 8 caracteres\n";
     return false;
 }
 
